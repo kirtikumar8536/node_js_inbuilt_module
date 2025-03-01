24b9e8b664ff2daf37b25743e8da8f90f895d06b
kirtikumar8536 <kirtikumar8536@gmail.com>
2025-03-02 00:30:30 +0530

    morgan+ejs basics

--- fullstack/app.js
+++ fullstack/app.js
@@ -6,18 +6,59 @@ let app = express();
 let dotenv = require('dotenv'); // Import the dotenv package
 dotenv.config(); // Load environment variables from a .env file
 let port = process.env.PORT || 6700; // Set port from env variable or default to 6700
- 
 
- let categoryRouter = require('./src/controller/CategoryRouter');
- let productRouter = require('./src/controller/productRouter');
+let morgan = require('morgan');
+let fs = require('fs');
 
+let categoryRouter = require('./src/controller/CategoryRouter');
+let productRouter = require('./src/controller/productRouter');
+const { title } = require('process');
+
+
+// Apply Morgan Middleware Before Routes
+// Move the app.use(morgan(...)) above the routes so that it executes before handling any request.
+
+// This line writes logs to app.log but overwrites the file on each request
+// app.use(morgan('combined', { stream: fs.createWriteStream('./app.log') }));
+
+// The following line ensures logs are appended to app.log instead of being overwritten on each request
+app.use(morgan('common')); // Logs to console
+app.use(morgan('common', { stream: fs.createWriteStream('./app.log', { flags: 'a' }) })); // Logs to file
+
+
+
+// Import Routers
+// Register Routers after middleware
 app.use('/category',categoryRouter);
 
 app.use('/product',productRouter);
 
 
+// Serve static files (CSS, JS, Images) from the 'public' folder
+app.use(express.static(__dirname + '/public'));
+
+/* 
+  Set the directory where HTML (EJS) files are stored.
+  By default, Express looks for views in a folder named "views", 
+  but here we are explicitly setting it to './src/views'.
+*/
+app.set('views', './src/views');
+
+/* 
+  Set EJS as the templating/view engine for rendering dynamic content.
+  This allows us to use `.ejs` files inside the 'views' directory.
+*/
+app.set('view engine', 'ejs');
+
+
+
+
+
+//default
 app.get('/',(req,res)=>{
-    res.send('Hi from express');
+   // res.send('Hi from express');
+   res.render('index', { title: 'Home Page', message: 'Welcome to EJS!' });
+  
 });
 
 
